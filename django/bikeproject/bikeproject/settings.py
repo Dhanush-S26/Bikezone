77f9ce7ab3c0345bc9365af7e3a7eea98d791591
DHANUSH S <dhanush260202@gmail.com>
2025-10-15 15:50:53 +0530

    Update settings.py

--- django/bikeproject/bikeproject/settings.py
+++ django/bikeproject/bikeproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bikeapp',
         'USER' : 'postgres',
-        'PASSWORD' : 'mcc@2020',
+        'PASSWORD' : 'your_db_password',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
